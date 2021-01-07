294b6d61af260ce7774f56a8ee9399bac627445a
matthewadamson <71205586+matthewadamson@users.noreply.github.com>
2021-01-07 05:33:37 -0600

    Adding JavaScript expressions inside my HTML

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const luckyNumber = Math.floor(Math.random() * 10);
+const fName = "Matt";
+const lName = "Adamson";
+
 ReactDOM.render(
   <div>
-    <h1>Matt's List</h1>
+    <h1>
+      {fName} {lName}'s List and lucky number
+    </h1>
+    <p>My lucky number is {luckyNumber}.</p>
     <ul>
       <li>Item 1</li>
       <li>Item 2</li>
